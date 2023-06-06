9e1618fdcf2bb6b0ac6507861cf9e02325077dd2
Boran-Boysan <123547574+Boran-Boysan@users.noreply.github.com>
2023-06-06 11:14:35 +0300

    Add files via upload

---
+++ war/Player.py
@@ -0,0 +1,82 @@
+import random
+
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
+ranks = ("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
+values = {"Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":11, "Queen":12, "King":13, "Ace":14}
+
+class Card():
+
+    def __init__(self,suit,rank):
+
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+
+        return self.rank + " of " + self.suit
+
+class Deck():
+
+    def __init__(self):
+
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit,rank)
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+class Player():
+
+    def __init__(self,name):
+        
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        
+        return self.all_cards.pop(0)
+
+    def add_cards(self,new_cards):
+        
+        if type(new_cards) == type([]):
+
+            self.all_cards.extend(new_cards)
+
+        else:
+
+            self.all_cards.append(new_cards)
+
+
+    def __str__(self):
+
+        return "Player {} has {} cards.".format(self.name,len(self.all_cards))
+
+
+new_deck = Deck()
+new_deck.shuffle()
+
+new_player = Player("Boran")
+
+print(new_player)
+
+mycard = new_deck.deal_one()
+print(mycard)
+"""
+new_player.add_cards(mycard)
+print(new_player)
+print(new_player.all_cards[0])
+
+"""
+new_player.add_cards([mycard,mycard,mycard])
+print(new_player)
+new_player.remove_one()
+print(new_player)
+
